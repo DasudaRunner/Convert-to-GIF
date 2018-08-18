382563fe0e6620905498dd1aa9d4f86bae45df4d
dasuda <dasuda2015@163.com>
2018-08-18 13:16:56 +0800

    a

--- video2gif.sh
+++
@@ -1,7 +0,0 @@
-filename=$1 
-sudo rm -rf ./jpegs/*
-mplayer -ao null ${filename} -vo jpeg:outdir=./jpegs
-convert ./jpegs/*.jpg -resize 45% ./out_big.gif
-sudo rm -rf ./jpegs/*
-convert -layers Optimize out_big.gif out.gif
-rm -rf ./out_big.gif
