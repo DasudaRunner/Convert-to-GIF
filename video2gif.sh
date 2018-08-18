3cac47e552be3a968b933644c69dbf5e6251862e
dasuda <dasuda2015@163.com>
2018-08-18 12:49:24 +0800

    repair some problem.

--- video2gif.sh
+++ video2gif.sh
@@ -1,15 +1,7 @@
-# David Wang 2018.01.22 in CUHK,Shenzhen
-# need kazam:installed in ubuntu sofeware,get video
-#      mplayer:sudo apt-get install mplayer,get jpegs
-#      imagemagic:sudo apt-get install imagemagic,get gif
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# this file should be with video file
-# input:video file name without '.mp4'
-# output:gif file
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-filename=$1  
+filename=$1 
 sudo rm -rf ./jpegs/*
-mplayer -ao null ${filename}.mp4 -vo jpeg:outdir=./jpegs
-convert ./jpegs/*.jpg -resize 45% ./out.gif
+mplayer -ao null ${filename} -vo jpeg:outdir=./jpegs
+convert ./jpegs/*.jpg -resize 45% ./out_big.gif
 sudo rm -rf ./jpegs/*
+convert -layers Optimize out_big.gif out.gif
+rm -rf ./out_big.gif
